fdb63dd7ea260d679fb47dd70eee373cea613462
zikotech76-a11y <zikotech76@gmail.com>
2026-07-05 19:31:51 +0100

    Initialize Firebase messaging service in SW

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,25 @@
+// firebase-messaging-sw.js
 
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "ضع_apiKey_هنا",
+  authDomain: "ضع_authDomain_هنا",
+  projectId: "ضع_projectId_هنا",
+  storageBucket: "ضع_storageBucket_هنا",
+  messagingSenderId: "ضع_messagingSenderId_هنا",
+  appId: "ضع_appId_هنا"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload.notification.title || "رسالة جديدة";
+  const options = {
+    body: payload.notification.body || "",
+    icon: "https://cdn-icons-png.flaticon.com/512/2462/2462719.png",
+    badge: "https://cdn-icons-png.flaticon.com/512/2462/2462719.png"
+  };
+  self.registration.showNotification(title, options);
+});
