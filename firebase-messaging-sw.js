18251a1180b132e7441ff45af0f1bc3e6ef6b9a3
zikotech76-a11y <zikotech76@gmail.com>
2026-07-05 19:43:24 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "ضع_apiKey_هنا",
-  authDomain: "ضع_authDomain_هنا",
-  projectId: "ضع_projectId_هنا",
-  storageBucket: "ضع_storageBucket_هنا",
-  messagingSenderId: "ضع_messagingSenderId_هنا",
-  appId: "ضع_appId_هنا"
+  apiKey: "AIzaSyBfHjM3zmUHEhr7ACra8GzKNx9z3jKfArI",
+  authDomain: "chatoline-9078a.firebaseapp.com",
+  projectId: "chatoline-9078a",
+  storageBucket: "chatoline-9078a.firebasestorage.app",
+  messagingSenderId: "494131536754",
+  appId: "1:494131536754:web:need_to_add_from_console"
 });
 
 const messaging = firebase.messaging();
